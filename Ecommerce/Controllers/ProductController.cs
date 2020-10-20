e91e7c7ba7236aad25c66dc3838dc2a082cb920a
gerardo lanza <glanza007@gmail.com>
2020-10-20 14:23:07 -0600

    Export Excel

--- Ecommerce/Controllers/ProductController.cs
+++ Ecommerce/Controllers/ProductController.cs
@@ -8,6 +8,8 @@ using Ecommerce.Helpers;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using OfficeOpenXml.Table;
 
 namespace Ecommerce.Controllers
 {
@@ -278,6 +280,28 @@ namespace Ecommerce.Controllers
         }
 
 
+        public IActionResult ExportarExcel()
+        {
+            string excelContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            var productos = _context.Products.AsNoTracking().ToList();
+            using (var libro = new ExcelPackage())
+            {
+                var worksheet = libro.Workbook.Worksheets.Add("Productos");
+                worksheet.Cells["A1"].LoadFromCollection(productos, PrintHeaders: true);
+                for (var col = 1; col < productos.Count + 1; col++)
+                {
+                    worksheet.Column(col).AutoFit();
+                }
+
+                // Agregar formato de tabla
+                var tabla = worksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: productos.Count + 1, toColumn: 5), "Productos");
+                tabla.ShowHeader = true;
+                tabla.TableStyle = TableStyles.Light6;
+                tabla.ShowTotal = true;
+
+                return File(libro.GetAsByteArray(), excelContentType, "Productos.xlsx");
+            }
+        }
     }
 
 

dbe9d1a7b1831ccc8d8d6fa7551ed43c33febeee
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-03-12 14:42:03 -0600

    Commit First

---
+++ ECommerce/ECommerce/Models/Departments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Departments
+    {
+       [Key]
+        public int DepartmentId { get; set; }
+
+        
+        [Required(ErrorMessage = "El Campo es requerido")]
+        [StringLength(30, ErrorMessage =
+     "The field {0} can contain maximun {1} and minimum {2} characters",
+     MinimumLength = 3)]
+        [Display(Name = "Depto")]
+        public string Name { get; set; }
+
+
+
+    }
+}
